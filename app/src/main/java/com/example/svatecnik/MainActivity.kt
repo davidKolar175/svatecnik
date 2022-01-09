a3f616a9be9da4d9616bdc928eb4d08d6e3f50ea
Kolář David <KolarD@tescosw.cz>
2022-01-09 15:08:13 +0100

    progress

--- app/src/main/java/com/example/svatecnik/MainActivity.kt
+++ app/src/main/java/com/example/svatecnik/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.svatecnik
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val homeFragment = HomeFragment()
-    //private val alarmsFragment = AlarmsFragment()
+    private val alarmsFragment = AlarmsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         replaceFragment(homeFragment)
-
-
+        bottom_navbar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_home -> replaceFragment(homeFragment)
+                R.id.ic_alarms -> replaceFragment(alarmsFragment)
+            }
+            true
+        }
+        //bottom_navbar
     }
 
     private fun replaceFragment(fragment: Fragment){
